5e40bf221a864a7efc123c686b43de3bc8fb51b9
oid <42318168@qq.com>
2018-05-23 16:27:06 +0800

    修改UIButton Category

--- Frameworks/YHCategorySDK.framework/Headers/UIButton+YH.h
+++ Frameworks/YHCategorySDK.framework/Headers/UIButton+YH.h
@@ -9,19 +9,7 @@
 
 @interface UIButton (YH)
 
-/** 图片在左，标题在右 */
-- (void)setIconInLeft;
-/** 图片在右，标题在左 */
-- (void)setIconInRight;
-/** 图片在上，标题在下 */
-- (void)setIconInTop;
-/** 图片在下，标题在上 */
-- (void)setIconInBottom;
-
-//** 可以自定义图片和标题间的间隔 */
-- (void)setIconInLeftWithSpacing:(CGFloat)Spacing;
-- (void)setIconInRightWithSpacing:(CGFloat)Spacing;
-- (void)setIconInTopWithSpacing:(CGFloat)Spacing;
-- (void)setIconInBottomWithSpacing:(CGFloat)Spacing;
+@property(nonatomic,assign)CGRect titleRect;
+@property(nonatomic,assign)CGRect imageRect;
 
 @end
